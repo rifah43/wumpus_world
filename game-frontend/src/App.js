6ea21ba3cfc1f650ea01f744ab2827df356da244
Rifah Tashfiha Faria <60043129+rifah43@users.noreply.github.com>
2023-09-23 22:31:18 +0600

    ok

--- game-frontend/src/App.js
+++ game-frontend/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App" >
       <header className="App-header">
+        Welcome to Wumpus World!
         <div className="board-container">
           <Board />
         </div>
